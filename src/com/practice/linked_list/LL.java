e1132dd7c3378bd00715b49eb0078b78c406c1a5
Ansari,Salman Sohrab <salmansohrab.ansari@sky.uk>
2023-11-14 18:03:40 +0530

    Singly Linked List

--- src/com/practice/linked_list/LL.java
+++ src/com/practice/linked_list/LL.java
@@ -21,13 +21,23 @@ public class LL {
         size++;
     }
 
-    public void display() {
+    public void display() {/*
         Node temp = head;
         while (temp != null) {
-            System.out.print(temp.value + "->");
+            System.out.print(temp.value + "-> ");
             temp = temp.next;
         }
-        System.out.print("END");
+        System.out.print("END ");*/
+        displayUsingRecursion(head);
+    }
+
+    public void displayUsingRecursion(Node head) {
+        Node temp = head;
+        if (temp == null){
+            return;
+        }
+        System.out.print(temp.value+"-> ");
+        displayUsingRecursion(temp.next);
     }
 
     public void insertLast(int val) {
@@ -41,19 +51,76 @@ public class LL {
     }
 
     public void insertAtNPosition(int position, int val) {
+        if (position == size) {
+            insertLast(val);
+        }
+        if (position == 0) {
+            insertFirst(val);
+        }
         Node temp = head;
         int index = 0;
         while (index < position - 1) {
             temp = temp.next;
             index++;
         }
-        Node node = new Node(val);
-        node.next = temp.next;
+        Node node = new Node(val, temp.next);
         temp.next = node;
+        size++;
+    }
+
+    public int deleteFirst() {
+        int deletedValue = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return deletedValue;
+    }
+
+    public int deleteLast() {
+        if (size <= 1) {
+            deleteFirst();
+        }
+        Node secondLast = getNode(size - 2);
+        int deletedValue = tail.value;
+        tail = secondLast;
+        secondLast.next = null;
+        return deletedValue;
+    }
+
+    public int deleteFromIndex(int index) {
+        if (index == 0) {
+            deleteFirst();
+        }
+        if (index == size - 1) {
+            deleteLast();
+        }
+        Node previous = getNode(index - 1);
+        int deletedValue = previous.next.value;
+        previous.next = previous.next.next;
+        return deletedValue;
+    }
+
+    private Node getNode(int index) {
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
     }
 
     public void reverse() {
+        reverseUsingRecursion(head);
+    }
 
+    private void reverseUsingRecursion(Node head) {
+        Node temp = head;
+        if (temp == null) {
+            return;
+        }
+        reverseUsingRecursion(temp.next);
+        System.out.print(temp.value + "-> ");
     }
 
     private class Node {
--- src/com/practice/linked_list/Main.java
+++ src/com/practice/linked_list/Main.java
@@ -8,8 +8,19 @@ public class Main {
         linkedList.insertFirst(8);
         linkedList.insertFirst(17);
         linkedList.insertLast(81);
-        linkedList.insertAtNPosition(3,17);
+        linkedList.insertAtNPosition(3, 17);
+        linkedList.insertAtNPosition(2, 55);
         linkedList.display();
-
+        System.out.println();
+        System.out.println(linkedList.deleteFirst());
+        linkedList.display();
+        System.out.println();
+        System.out.println(linkedList.deleteLast());
+        linkedList.display();
+        System.out.println();
+        System.out.println(linkedList.deleteFromIndex(3));
+        linkedList.display();
+        System.out.println();
+        linkedList.reverse();
     }
 }
